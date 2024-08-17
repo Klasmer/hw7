ab9e3b104c05f320b08cf43787c58dcc2ee7f855
klasmer <klasmer495@gmail.com>
2024-08-17 15:20:51 +0300

    Циклы Урок 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //С помощью цикла while посчитайте, сколько месяцев потребуется, чтобы накопить 2 459 000 рублей при условии,
+        // что первоначально мы имеем 0 рублей и готовы откладывать по 15 тысяч рублей.
+        System.out.println("Задача 1");
+        int total = 0;
+        int savingsPerMonth = 15_000;
+        int month = 0;
+        int bank = 2_459_000;
+        while (total < bank) {
+            total = total + savingsPerMonth;
+            month++;
+            System.out.println("Месяц " + month + " сумма накоплений = " + total + " рублей");
+        }
+
     }
 }
